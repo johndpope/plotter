58994b490e7074460a349e1d459d820ae185f9cb
William Emfinger <waemfinger@gmail.com>
2017-04-10 08:51:35 -0700

    updated to properly load plot.html to prepare for loading multiple logs.

--- renderer.js
+++ renderer.js
@@ -4,7 +4,7 @@ var dialog = remote.dialog;
 var path = require('path');
 var _ = require('lodash');
 var fs = require('fs');
-var plotHtml = fs.readFileSync('plot.html');
+var plotHtml = fs.readFileSync('plot.html').toString();
 
 var requirejs = require('requirejs');
 
@@ -30,9 +30,9 @@ function removeExtension(filename){
 }
 
 dialog.showOpenDialog(function (fileNames) {
-		if(fileNames === undefined)
-				console.log("No file selected");
-		else {
+    if(fileNames === undefined)
+	console.log("No file selected");
+    else {
         console.log("Opened " + fileNames);
         fs.readFile(fileNames[0], 'utf-8', function(err, data) {
             if (err) {
@@ -43,18 +43,18 @@ dialog.showOpenDialog(function (fileNames) {
             // init vars
             var a = fileNames[0],
                 datas = {},
-	              first_time = undefined,
-	              last_time = undefined;
+	        first_time = undefined,
+	        last_time = undefined;
 
             // Prepare hide plot func
             var hidePlotFunc = function(a) {
-		            var active = datas[a].active ? false : true;
-		            var opacity = active ? 0 : 1;
-		            var visibility = active ? 'hidden' : 'visible';
-		            var display = active ? 'none' : 'block';
-		            d3.select('#plot_'+a)
-		                .style('display', display);
-		            datas[a].active = active;
+		var active = datas[a].active ? false : true;
+		var opacity = active ? 0 : 1;
+		var visibility = active ? 'hidden' : 'visible';
+		var display = active ? 'none' : 'block';
+		d3.select('#plot_'+a)
+		    .style('display', display);
+		datas[a].active = active;
             };
 
             // parse the log
@@ -63,52 +63,49 @@ dialog.showOpenDialog(function (fileNames) {
                 parsed = userparser.getDataFromAttribute(data);
             datas[a] = parsed;
             var aliases = Object.keys(datas[a]);
-			      aliases.map((key) => {
-				        var d = datas[a][key].data;
-				        var first_entry = d[0];
-				        var last_entry = d[d.length-1];
-				        if ( first_time === undefined || first_time > first_entry[0] ) {
-				            first_time = first_entry[0];
-				        }
-				        if ( last_time === undefined || last_time < last_entry[0] ) {
-				            last_time = last_entry[0];
-				        }
-			      });
-            // plot the logs
-		        for (var a in datas) {
-			          // setup the html
-                /*
-                console.log($('#main'));
-			          $('#main').append(plotHtml);
-                 */
-			          var container = $('#log');
-			          $(container).attr('id', 'log_'+a);
-
-			          var title = $(container).find('#title');
-			          $(title).attr('id','title_'+a)
-			              .on('click', function(_a) {
-				                return function() {
-				                    hidePlotFunc(_a);
-				                };
-			              }(a));
-
-			          title.append('<b>'+a+'</b>');
-
-			          var p = $(container).find('#plot');
-			          $(p).attr('id',"plot_" + a);
-
-			          var data = datas[a];
-			          var offset = first_time;
-			          if (!_.isEmpty(data)) {
-			              var aliases = Object.keys(data);
-			              aliases.map((key) => {
-				                data[key].data.push([last_time, 0]);
-			              });
-			              plotter.plotData('plot_'+a, data, offset);
-			          }
-			          else
-			              $(container).detach();
-		        }
-        });
+	    aliases.map((key) => {
+		var d = datas[a][key].data;
+		var first_entry = d[0];
+		var last_entry = d[d.length-1];
+		if ( first_time === undefined || first_time > first_entry[0] ) {
+		    first_time = first_entry[0];
 		}
+		if ( last_time === undefined || last_time < last_entry[0] ) {
+		    last_time = last_entry[0];
+		}
+	    });
+            // plot the logs
+	    for (var a in datas) {
+		// setup the html
+		$(plotHtml).appendTo('#main');
+		var container = $('#log');
+		$(container).attr('id', 'log_'+a);
+
+		var title = $(container).find('#title');
+		$(title).attr('id','title_'+a)
+		    .on('click', function(_a) {
+			return function() {
+			    hidePlotFunc(_a);
+			};
+		    }(a));
+
+		title.append('<b>'+a+'</b>');
+
+		var p = $(container).find('#plot');
+		$(p).attr('id',"plot_" + a);
+
+		var data = datas[a];
+		var offset = first_time;
+		if (!_.isEmpty(data)) {
+		    var aliases = Object.keys(data);
+		    aliases.map((key) => {
+			data[key].data.push([last_time, 0]);
+		    });
+		    plotter.plotData('plot_'+a, data, offset);
+		}
+		else
+		    $(container).detach();
+	    }
+        });
+    }
 });
